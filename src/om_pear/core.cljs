5a0701d1ee2731fbdbaa456a31776c52b66e2149
Greg Berenfield <gberenfield@berenfield.com>
2014-07-22 11:03:38 -0400

    move config into atom

--- src/om_pear/core.cljs
+++ src/om_pear/core.cljs
@@ -10,6 +10,7 @@
                    :data
                    (clj->js [{:name "Bob Fleming" :address "230 Oak St., NJ USA"}
                           {:name "Rick Rearden" :address "91 Windings Way, Clement OK 28174"}])
+                   :config (clj->js {:AllowColumnResize true :ShowCellBorder true :AllowAlternateRowHighlight true})
                    }))
 
 (defn ^:export om-pear [app-state owner]
@@ -17,7 +18,7 @@
                       dom-ele (dom/div #js {:id "grid2"})]
                   (.setColumns g (:columns @mygrid))
                   (.setDataRows g (:data @mygrid))
-                  (.setConfiguration g #js {:AllowColumnResize true :ShowCellBorder true :AllowAlternateRowHighlight true})
+                  (.setConfiguration g (:config @mygrid) )
                   (.setWidth g 500)
                   (.setHeight g 200)
                   (.render g (. js/document (getElementById "grid2")))
